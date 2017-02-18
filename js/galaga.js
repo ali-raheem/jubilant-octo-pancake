2b37e4a33bbe4e0c4511751b67aeda05bcaf9c98
Ali Raheem <ali@theraheemfamily.co.uk>
2017-02-18 17:29:01 +0000

    Added working collision with bullets

--- js/galaga.js
+++ js/galaga.js
@@ -11,6 +11,10 @@ class Sprite {
     render() {
         this.ctx.drawImage(this.image, this.x, this.y);
     }
+    static collides (A, B) {
+        return (A.x + A.width > B.x && A.x < B.x + B.width &&
+                A.y + A.height > B.y && A.y < B.y + B.height);
+    }
 }
 
 class animatedSprite extends Sprite {
@@ -52,6 +56,7 @@ class Player extends Sprite {
         super(ctx, image, x, y, width, height, speed);
         this.bullets = 5;
         this.gun = greenLaser;
+        this.alive = true;
     }
 }
 
@@ -101,6 +106,7 @@ function gameSetup(canvasId) {
         1
     );
 
+    running = true;
     loop();
 }
 
@@ -109,14 +115,31 @@ function loop() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     animateBullets();
     animateEnemies();
-    player.render();    
-    requestAnimationFrame(loop);
+    player.render();
+    if(enemies.length == 0){
+        ctx.fillStyle = 'white';
+        ctx.font = '48px Serif';
+        ctx.fillText("You win!", canvas.width/2, canvas.height/2);
+        running = false;
+    }
+    if(player.alive == false) {
+        ctx.fillStyle = 'red';
+        ctx.font = '48px Serif';
+        ctx.fillText("You died!", canvas.width/2, canvas.height/2);
+        running = false;
+    }
+    if(running)
+        requestAnimationFrame(loop);
 }
 
 function animateBullets() {
     for (i = 0; i < bullets.length; i++) {
         bullets[i].update();
         bullets[i].render();
+        if(Sprite.collides(player, bullets[i])){
+            player.alive = false;
+            break;
+        }
         if(bullets[i].y + bullets[i].height <= 0 || bullets[i].y >= canvas.height) {
             bullets.splice(i, 1);
         }
@@ -124,11 +147,13 @@ function animateBullets() {
 }
 
 function addBullet () {
+    if(bullets.length >= player.bullets)
+        return;
     var bullet = new Bullet(
         ctx,
         greenLaser,
         player.x + player.width/2 - 4,
-        player.y + player.height/2,
+        player.y - greenLaser.height,
         greenLaser.width,
         greenLaser.height,
         3
@@ -141,7 +166,7 @@ function addEnemyBullet (enemy) {
         ctx,
         redLaser,
         enemy.x + enemy.width/2 - 4,
-        enemy.y + enemy.height/2,
+        enemy.y + enemy.height,
         redLaser.width,
         redLaser.height,
         -3
@@ -169,25 +194,17 @@ function animateEnemies() {
             enemies[i].update();
             enemies[i].render();
             // Random chance enemy will shoot.
-            if(Math.random() <= 0.001) {
+            if(Math.random() <= 0.001)
                 addEnemyBullet(enemies[i]);
+            if(0 < bullets.length) {
+                for(j = 0; j < bullets.length; j++) {
+                    if(Sprite.collides(bullets[j], enemies[i])){
+                        enemies.splice(i, 1);
+                        bullets.splice(j, 1);
+                        break;
+                    }
+                }
             }
-//            for (j = 0; i < bullets.length; j++) {
-//                if(rectCollision(enemies[i], bullets[j])) {
-//                    enemies[i].alive = false;
-//                    break;
-//                }
-//            }
         }
     }
-}
-
-
-
-
-function rectCollision(rect1, rect2) {
-    return ((rect1.x < rect2.x + rect2.width) &&
-            (rect1.x + rect1.width > rect2.x) &&
-            (rect1.y < rect2.y + rect2.height) &&
-            (rect1.height + rect1.y > rect2.y));
 }
